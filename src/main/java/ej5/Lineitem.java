10689909bf96272b96c307596ca043b0f2dcdd5f
Hanthu64 <146928244+Hanthu64@users.noreply.github.com>
2024-10-07 17:45:51 +0200

    Delete src/main/java/ej5 directory

--- src/main/java/ej5/Lineitem.java
+++
@@ -1,8 +0,0 @@
-package ej5;
-
-import java.math.BigDecimal;
-
-public class Lineitem {
-    private int quantity;
-    private BigDecimal price;
-}
--- src/main/java/ej5/Shopping_Cart.java
+++
@@ -1,7 +0,0 @@
-package ej5;
-
-import java.util.Date;
-
-public class Shopping_Cart {
-    private Date created;
-}
